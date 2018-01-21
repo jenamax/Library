7374289046a59475f3b71f8f1a68152fc368ecd9
evgeniy <jenamax@mail.ru>
2018-01-21 13:03:41 +0300

    1

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
+
+import static sun.net.www.protocol.http.AuthCacheValue.Type.Server;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
+}
