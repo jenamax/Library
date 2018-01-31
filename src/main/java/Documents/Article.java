4f6d23d56569ca77988749974fcd9ad580cc8b98
evgeniy <jenamax@mail.ru>
2018-01-31 15:34:55 +0300

    Type of keys and authors of Documents changed String[] -> ArrayList<String>

--- src/main/java/Documents/Article.java
+++ src/main/java/Documents/Article.java
@@ -1,8 +1,10 @@
 package Documents;
 
+import java.util.ArrayList;
+
 public class Article extends Document {
 
-    public Article(String title, int price, String[] author, String[] keys) {
+    public Article(String title, int price, ArrayList<String> author, ArrayList<String> keys) {
         super(title, price, author, keys);
     }
 
