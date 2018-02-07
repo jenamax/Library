e287b4ab173b2abf25c27e7e4bbf0a3bd675a25b
SimonWT <y.yudinskih@innopolis.ru>
2018-02-07 14:19:03 +0300

    DA NU VSE V ZHOPU

--- src/main/java/TestSuit/TestCase8.java
+++ src/main/java/TestSuit/TestCase8.java
@@ -1,7 +1,21 @@
 package TestSuit;
 
+import Documents.Book;
+import Users.Librarian;
+import Users.Patron;
+
 /**
  * Created by simon on 2/7/2018.
  */
 public class TestCase8 {
+    Librarian librarian = new Librarian();
+    Patron f = new Patron();
+    f.setType("faculty");
+    Patron s = new Patron();
+    s.setType("student");
+    Book b = new Book();
+    b.setBestSeller(true);
+    b.setCopies(1);
+    s.checkout(b);
+    System.out.println(s.documents.get(0).daysRemained);
 }
