766590c55a9db7f3bc34e76b2f11e2d493b4ed04
SimonWT <y.yudinskih@innopolis.ru>
2018-02-07 14:28:14 +0300

    TestCase 8

--- src/main/java/TestSuit/TestCase8.java
+++ src/main/java/TestSuit/TestCase8.java
@@ -1,7 +1,27 @@
 package TestSuit;
 
+import Documents.Book;
+import Users.Librarian;
+import Users.Patron;
+
 /**
  * Created by simon on 2/7/2018.
  */
 public class TestCase8 {
+    public static void main(String[] args) {
+        Librarian librarian = new Librarian();
+        librarian.setName("LIBRARIAN");
+        Patron f = new Patron();
+        f.setType("faculty");
+        f.setName("FACULTY");
+        Patron s = new Patron();
+        s.setType("student");
+        s.setName("STUDENT");
+
+        Book b = new Book();
+        b.setDoc(5, "How to take A in ITP", 1488, "A.Naumchev", "");
+        b.setCopies(1);
+        s.checkout(b);
+        System.out.println(s.documents.get(0).daysRemained+ " days");
+    }
 }
